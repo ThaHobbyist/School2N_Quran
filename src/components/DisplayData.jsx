a23a27f9bccef0409198ace74e3e04703ecb7990
ThaHobbyist <dipdeys.dey@gmail.com>
2023-01-13 02:54:19 +0530

    Added ReadSurah page

---
+++ src/components/DisplayData.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ReactComponent as Des } from "../assets/num.svg";
+import Des2 from "../assets/num.png";
+
+import Quran from "../utils/Quran.json";
+import Quran_translated from "../utils/Quran_translated.json";
+import colorSet from "../utils/colorSet";
+
+export default function DisplayData() {
+	const colors = colorSet["colors"];
+	const Q = Quran["data"];
+	const Q_t = Quran_translated["data"];
+
+	return (
+		<>
+			<Display>
+				{Q["surahs"].map((item) => {
+					return (
+						<div
+							className="data"
+							key={item.number}
+							style={{
+								backgroundColor:
+									colors[Math.floor(Math.random() * 4)],
+							}}
+						>
+							<div className="number">
+								{/* <Des /> */}
+								<img className="image" src={Des2} alt="" />
+								<p className="num">{item.number}</p>
+							</div>
+							<div className="heading">
+								<h5>{item.name}</h5>
+							</div>
+							<div className="translation">
+								<h6>{item.englishName}</h6>
+								<p>{item.englishNameTranslation}</p>
+							</div>
+						</div>
+					);
+				})}
+			</Display>
+		</>
+	);
+}
+
+const Display = styled.div`
+	height: 68vh;
+	min-width: 60vw;
+	overflow-y: scroll;
+	padding: 0 2.5rem;
+
+	.data {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		margin: 2rem 0;
+
+		border: 3px solid #000000;
+		box-shadow: 2px 2px 0px #000000;
+		border-radius: 20px;
+		padding: 2rem 3rem;
+
+		color: white;
+
+		.heading {
+			margin: 0 3rem;
+
+			h5 {
+				font-family: "Amiri", serif;
+				font-size: 4rem;
+				margin: 0;
+			}
+		}
+
+		.number {
+			position: relative;
+			.image {
+				width: 100%;
+				height: 100%;
+			}
+
+			.num {
+				position: absolute;
+				top: 0;
+				left: 0;
+				bottom: 0;
+				right: 0;
+			
+				height: fit-content;
+				margin: auto;
+				text-align: center;
+
+				font-family: "Poppins";
+				font-style: normal;
+				font-weight: 700;
+				font-size: 30px;
+			}
+		}
+
+		.translation {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			h6 {
+				font-family: "K2D";
+				font-style: normal;
+				font-weight: 400;
+				font-size: 2.2rem;
+				margin: 0;
+
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+
+			h5 {
+				margin: 0;
+				font-family: "K2D";
+				font-style: normal;
+				font-weight: 400;
+				font-size: 2.5rem;
+			}
+		}
+	}
+
+	&::-webkit-slider-thumb {
+		
+		width: 12px;
+		border: 2px solid #000;
+		border-radius: 50%;
+		background-color: #33ff00;
+	}
+
+	&::-webkit-slider-runnable-track {
+		
+		width: 16px;
+		border: 3px solid #000;
+		border-radius: 50%;
+		background-color: #fff;
+	}
+`;
--- src/pages/ReadSurah.jsx
+++ src/pages/ReadSurah.jsx
@@ -4,11 +4,11 @@ import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Back from "../components/Back";
 import Heading from "../components/Heading";
-
-import Quran from '../utils/Quran.json';
-import Quran_translated from '../utils/Quran_translated.json';
+import DisplayData from "../components/DisplayData"
 
 function ReadSurah() {
+
+
 
 	return (
 		<>
@@ -16,12 +16,26 @@ function ReadSurah() {
 				<Container>
 					<Heading />
 					<Back />
-                    <div className="content">
-                        <div className="title">
-                            <h5>Learning Quran {'>'} </h5>
-                            <h6>Surah Wise</h6>
-                        </div>
-                    </div>
+					<div className="content">
+						<div className="title">
+							<h5>Learning Quran {">"} </h5>
+							<h6>Surah Wise</h6>
+						</div>
+						<div className="body">
+							<DisplayData />
+							<div className="def">
+								<h5>Surah:</h5>
+								<h6>
+									A surah, is the equivalent of "chapter" in
+									the Qur'an. There are 114 surahs in the
+									Quran, each divided into ayats. The chapters
+									or surahs are of unequal length; the
+									shortest surah has only three verses while
+									the longest contains 286 verses.
+								</h6>
+							</div>
+						</div>
+					</div>
 				</Container>
 				<Navbar />
 			</Bg>
@@ -82,12 +96,46 @@ const Container = styled.div`
 				font-weight: 700;
 				font-size: 3.125rem;
 				text-transform: capitalize;
-                margin: 0 0 0 1rem;
+				margin: 0 0 0 1rem;
 
-                color: #fff;
+				color: #fff;
 
 				-webkit-text-stroke: 2px #000;
 				text-shadow: 1px 1px 0px #000000;
+			}
+		}
+
+		.body {
+			display: flex;
+
+			.def {
+				width: 35vw;
+				padding: 0 2rem;
+				
+
+				h5 {
+					font-family: "K2D";
+					font-style: normal;
+					font-weight: 600;
+					font-size: 2.5rem;
+					margin: 0;
+					text-transform: capitalize;
+					color: #fff;
+					-webkit-text-stroke: 1px #000;
+					text-shadow: 1px 1px 0px #000000;
+				}
+
+				h6 {
+					font-family: "K2D";
+					font-style: normal;
+					font-weight: 400;
+					font-size: 1.8rem;
+					margin: 0;
+					
+
+					/* -webkit-text-stroke: 0.5px #000;
+					text-shadow: 1px 1px 0px #000000; */
+				}
 			}
 		}
 	}
